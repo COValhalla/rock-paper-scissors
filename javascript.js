bcaaf469a9970ead85a1f2476929645ba41c2529
Joe <josephvellella@gmail.com>
2022-05-12 14:17:40 -0600

    Add playerSelection to receive players input.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,23 @@
-// create a function computerPlay
-
+// create a function computerPlay that generates the computer's move
 function computerPlay(){
     //Create an integer between 0 and 2
     result = Math.floor(Math.random()*3)
     //Assign random integer to each value and return the result
     if (result === 0){
-        return 'Rock'
+        return 'rock'
     }else if (result === 1){
-        return 'Paper'
+        return 'paper'
     }else {
-        return 'Scissors'
+        return 'scissors'
     }
-    //Output the result to the console
 }
-// randomly generate a value 'rock', 'paper', 'scissors'
+
+//Takes players input and only accepts rock, paper, or scissors.
+function playerSelection() {
+    let userInput;
+    while (userInput != 'rock' && userInput != 'paper' && userInput != 'scissors'){
+        userInput = prompt('Enter rock, paper, or scissors.')
+        userInput = userInput.toLowerCase();
+    }
+    return userInput;
+}
